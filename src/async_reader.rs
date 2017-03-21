bcec1723459a25f91002512261c641f9401911a1
pbougue <pierre-etienne.bougue@canaltp.fr>
2017-03-21 02:22:12 +0100

    correct reading of check output (#1)
    correct is_alphabetic for word_add

--- src/async_reader.rs
+++ src/async_reader.rs
@@ -3,32 +3,30 @@
 // this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use std::process::ChildStdout;
-use std::io::Read;
+use std::io::{BufReader, BufRead};
 use std::sync::mpsc::Sender;
 
 use error::Result;
 
-const BUF_LEN: usize = 42;
 
 /// An asynchronous reader, that reads from a spawned command stdout
 /// and sends it to a channel
 pub struct AsyncReader {
-    stdout: ChildStdout,
+    stdout: BufReader<ChildStdout>,
     sender: Sender<Result<String>>,
-
 }
 
 impl AsyncReader {
     /// Create a new AsyncReader
     pub fn new(stdout: ChildStdout, sender: Sender<Result<String>>) -> AsyncReader {
         AsyncReader {
-            stdout: stdout,
+            stdout: BufReader::new(stdout),
             sender: sender,
         }
     }
-    
+
     /// Reads the output from ispell and sends it over the channel
-    pub fn read_loop(&mut self)  {
+    pub fn read_loop(&mut self) {
         loop {
             let result = self.read();
             match self.sender.send(result) {
@@ -40,18 +38,14 @@ impl AsyncReader {
 
     /// Reads a string
     fn read(&mut self) -> Result<String> {
-        let mut buffer = [0; BUF_LEN];
-        let mut output = vec!();
+        let mut output = String::new();
         loop {
-            let n = try!(self.stdout.read(&mut buffer));
-            output.extend_from_slice(&buffer[0..n]);
-            if n < BUF_LEN {
+            try!(self.stdout.read_line(&mut output));
+            if output.ends_with("\n\n") || output == "\n" || output.starts_with("@") {
                 break;
-            } else {
-                continue;
             }
         }
-        let s = try!(String::from_utf8(output));
-        Ok(s)
+        Ok(output)
     }
 }
+
